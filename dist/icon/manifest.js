b089880089875b3a365464cf55533cee2a8807dd
MFerguson1449 <MFerguson1449@gmail.com>
2019-08-17 11:02:44 -0400

    new footer section

--- dist/icon/manifest.js
+++ dist/icon/manifest.js
@@ -180,7 +180,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "38777" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "35574" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
